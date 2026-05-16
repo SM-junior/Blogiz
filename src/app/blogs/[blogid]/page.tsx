96a3c0b9c67d2983178e1815f48f6aa6b6c7446c
SM-junior <smshahin12341@gmail.com>
2026-05-16 10:34:58 +0600

    redux setup done on /src/lib/Providers.tsx, /src/redux/store.ts and src/redux/api/baseApi.ts

--- src/app/blogs/[blogid]/page.tsx
+++ src/app/blogs/[blogid]/page.tsx
@@ -8,7 +8,7 @@ export interface BlogId {
 }
 
 export async function generateStaticParams() {                      //'npm run build' korar somoy jeno first 3 ta blog SSG hoye thake jate client click dile load nite n hoy
-    const res = await fetch('http://localhost:5000/blogs');
+    const res = await fetch('http://localhost:5000/blogs');            {/* mane holo amar 100 blogs theke first 3 ta blogs ami static kore rakci*/}
     const blogs = await res.json();
 
     return blogs.slice(0, 3).map((blog: Blog) => ({
